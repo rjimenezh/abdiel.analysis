014fd9ef86e045e69470f1471ebd83e9d8bf7d50
unknown <rjimenezh@gmail.com>
2014-09-20 18:27:02 -0400

    Documented class, refactored analysis methods to provide more concise interfaces by using model traversal to access wires from a port

--- src/abdiel/analysis/actions/CheckPortConnections.java
+++ src/abdiel/analysis/actions/CheckPortConnections.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.eclipse.emf.common.util.EList;
 
 import circuit.Circuit;
+import circuit.Part;
 import circuit.Port;
 import circuit.PortConnection;
 import circuit.PortWiring;
@@ -31,19 +32,26 @@ public class CheckPortConnections extends CircuitAnalysisAction {
 	 * role consistency (identical role
 	 * sets on both connected ports),
 	 * and wiring.
+	 * 
+	 * @param circuit ABDIEL circuit to analyze.
 	 */
 	@Override
 	public void analyze(Circuit circuit) {
-		EList<PortWiring> wires = circuit.getPortWires();
-		for(PortConnection conn : circuit.getPortConns()) {
-			Port source = conn.getSource();
-			Port target = conn.getTarget();
-			checkProtocols(source, target);
-			checkRoleConsistency(source, target, wires);
+		for(PortConnection conn : circuit.getPortConns()) {			
+			checkProtocols(conn);
+			checkRoleConsistency(conn);
 		}
 	}
 	
-	protected void checkProtocols(Port source, Port target) {
+	/**
+	 * Validates that the protocols of the ports in a port
+	 * connection match each other.
+	 * 
+	 * @param conn Port connection to analyze for protocol consistency
+	 */
+	protected void checkProtocols(PortConnection conn) {
+		Port source = conn.getSource();
+		Port target = conn.getTarget();
 		System.err.println("Checking " + source.getName() + "->" + target.getName()
 			+ " protocols...");
 		String sourceProto = source.getProtocol();
@@ -56,12 +64,21 @@ public class CheckPortConnections extends CircuitAnalysisAction {
 			System.err.println("Null protocol"); // flag error
 	}
 	
-	protected void checkRoleConsistency(Port source, Port target,
-	EList<PortWiring> wires) {
+	/**
+	 * Checks the port wiring of each port in a connection and
+	 * determines whether the wiring is consistent, i.e. whether
+	 * both ports export the same set of roles, and whether all
+	 * roles in both ports are properly wired to a concrete pin.
+	 * 
+	 * @param conn Port connection to analyze for protocol consistency
+	 */
+	protected void checkRoleConsistency(PortConnection conn) {
+		Port source = conn.getSource();
+		Port target = conn.getTarget();
 		System.err.println("Checking " + source.getName() + "->" + target.getName()
 			+ " roles...");
-		List<PortWiring> sourceWires = getWiresFor(wires, source);
-		List<PortWiring> targetWires = getWiresFor(wires, target);
+		List<PortWiring> sourceWires = getWiresFor(source);
+		List<PortWiring> targetWires = getWiresFor(target);
 		List<String> roles = new ArrayList<String>();
 		for(PortWiring srcWire : sourceWires) {
 			if(srcWire.getPin() == null)
@@ -77,7 +94,17 @@ public class CheckPortConnections extends CircuitAnalysisAction {
 			System.err.println("Role mismatch"); // flag error
 	}
 	
-	protected List<PortWiring> getWiresFor(EList<PortWiring> wires, Port port) {
+	/**
+	 * Finds and returns the list of port wirings attached
+	 * to a particular port.
+	 * 
+	 * @param port Port whose port wirings are desired
+	 * @return List of port wirings connected to the specified port
+	 */
+	protected List<PortWiring> getWiresFor(Port port) {
+		Part part = (Part)port.eContainer();
+		Circuit ckt = (Circuit)part.eContainer();
+		EList<PortWiring> wires = ckt.getPortWires();
 		List<PortWiring> wiresFor = new ArrayList<PortWiring>();
 		for(PortWiring eachWire : wires)
 			if(eachWire.getPort() == port)
